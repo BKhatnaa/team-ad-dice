72f8f6d1ed8f49e72ef3e18a6623a6895b31dc5b
rboxochir <erdeneochir0417@gmail.com>
2018-05-29 11:10:42 +0800

    Add files via upload

--- poly1.py
+++ poly1.py
@@ -1,20 +1,35 @@
-poly = [[0,1],[0,4],[2,4],[1,2],[3,2],[3,3],[4,2],[4,1]]
-pnt = [1,3]
-c = []
+poly = [[1,1],[1,2],[2,3],[3,2],[3,1]]
+pnt = [1,1]
+c = poly
 v = []
-for i in range(len(poly)):
-    if(poly[i][0] >= pnt[0]):
-        c.append(poly[i])
 for i in range(len(c)-1):
-	slope = (c[i][1]-c[i+1][1])/c[i][0]-c[i+1][0]
-	if slope == 0:
-		sh = 0
+	if(c[i][0] == pnt[0] and c[i][1] == pnt[1]):
+		print("oroin tseg")
 	else:
-		sh = (pnt[1]-c[i][1]+ (slope * c[i][0]))/slope
-	if(c[i][0] <= sh and sh <= c[i+1][0] or c[i+1][0] <= sh and sh <=c[i][0]):
-		if poly[i][1] != pnt[1]:
+		if(c[i][0]-c[i+1][0] == 0):
+			if(c[i][1] >= pnt[1] and c[i+1][1] <= pnt[1] or c[i+1][1] >= pnt[1] and c[i][1] <= pnt[1]):
+				sh = c[i][0]
+			else:
+				sh = 0
+		else:
+			slope = (c[i][1]-c[i+1][1])/(c[i][0]-c[i+1][0])
+			if slope == 0:
+				sh = 0
+			else:
+				#intersection point x
+				sh = (pnt[1]-c[i][1]+ (slope * c[i][0]))/slope
+				if(sh >=c[i][0] and sh <= c[i+1][0] or sh <= c[i][0] and sh >=c[i+1][0]):
+					sh = (pnt[1]-c[i][1]+ (slope * c[i][0]))/slope
+				else: 
+					sh = 0
+		#Check point		
+		if(sh == pnt[0] and pnt[1] == c[i][1]):
 			v.append("1")
-print(c)		
+		elif(sh >= pnt[0]):
+			v.append("1")
+print(c)
+print(len(v))
+#Check inside or outside		
 if(len(v) % 2 == 0):
 	print("outside")
 else:
