625d380e0cfd470a2deadb659b3aeade2a96a828
BKhatnaa <khatnaa3139@outlook.com>
2018-06-06 14:45:48 +0800

    shine

--- shinealgorithm.py
+++ shinealgorithm.py
@@ -1,29 +1,33 @@
-coor = [[0,1],[1,3],[2,1]]
-point = [1,2]
-zov = []
-l = []
-first = coor[0]
+coor = [[1,1],[1,4],[3,4],[3,2],[4,3],[5,2],[6,2],[6,3],[7,3],[7,1]]
+point = [2,2]
+si = []
 le = []
-coor.append(first)
+zov = []
+coor.append(coor[0])
 for i in coor:
     zov.append(i)
 def findarea(coordinate):
     for i in range(len(coordinate)-1):
-        sig = (coordinate[i][0]*coordinate[i+1][1] - coordinate[i][1]*coordinate[i+1][0])
-        l.append(sig)
-        area = abs(sum(l) / 2)
+        si.append((coordinate[i][0]*coordinate[i+1][1])-(coordinate[i][1]*coordinate[i+1][0]))
+        area = abs((sum(si)) / 2.0)
     print(area)
+    return area
 for i in range(len(coor)-1):
     leng = (((point[0]-coor[i][0])**2) + ((point[1]-coor[i][1])**2)**0.5)
     le.append(leng)
-for i in range(len(le)):
+for i in range(len(le)-1):
     if min(le)==le[i]:
-        zov.insert(i+1,point)
-        break
+        if le[i+1] > le[i-1]:
+            zov.insert(i-1,point)
+            break
+        else:
+            zov.insert(i+1, point)
+            break
 ehniih = findarea(coor)
 daraa = findarea(zov)
 print(coor)
 print(zov)
+daraa = daraa - ehniih
 if ehniih > daraa:
     print('dotor')
 else:
